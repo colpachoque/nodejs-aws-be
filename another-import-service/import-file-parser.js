0f8e124534ecdf62896bdb23d2fbd63ec9a5e6ef
Olga Khamanina <smile-94@inbox.ru>
2020-11-15 21:13:20 +0300

    Rewrite importSecondFileParser, now it moves file from uploaded to parsed folder

--- another-import-service/import-file-parser.js
+++ another-import-service/import-file-parser.js
@@ -3,40 +3,53 @@ import csv from 'csv-parser';
 
 import config from './config.js';
 
-const s3 = new AWS.S3({ region: config.aws.region });
+const s3 = new AWS.S3({ region: config.aws.region }),
+    collectDataForLogging = objKey => {
+        return new Promise((res, rej) => {
+            const results = [];
+
+            s3.getObject({
+                Bucket: config.aws.s3.bucket,
+                Key: objKey
+            }).createReadStream()
+                .on('error', e => {
+                    console.log(e);
+                    rej(e);
+                })
+                .pipe(csv())
+                .on('error', e => {
+                    console.log(e);
+                    rej(e);
+                })
+                .on('data', data => results.push(data))
+                .on('end', async () => res(results));
+            });
+    }
 
 export const importSecondFileParser = async event => {
     console.log('In import second file parser')
     for (const record of event.Records) {
         console.log(record.s3.object.key);
-        await new Promise ((res, rej) => {
-            s3.getObject({
+
+        const data = await collectDataForLogging(record.s3.object.key);
+        console.log('data ', data);
+        
+        /* Copy */
+        const initialPath = record.s3.object.key;
+        console.log(`Copy from ${config.aws.s3.bucket}/${record.s3.object.key}`);
+        await s3.copyObject({
             Bucket: config.aws.s3.bucket,
-            Key: record.s3.object.key
-        }).createReadStream()
-            .on('error', e => {
-                console.log(e);
-                rej(e);
-            })
-            .pipe(csv())
-            .on('error', e => {
-                console.log(e);
-                rej(e);
-            })
-            .on('data', data => console.log(data))
-            .on('end', async () => {
-                console.log(`Copy from ${config.aws.s3.bucket}/${record.s3.object.key}`);
+            CopySource: `${config.aws.s3.bucket}/${record.s3.object.key}`,
+            Key: record.s3.object.key.replace(config.aws.s3.uploadFolderName, config.aws.s3.parsedFolderName)
+        }).promise();
+        console.log(`Copied to ${config.aws.s3.bucket}/${config.aws.s3.parsedFolderName}`);
 
-                await s3.copyObject({
-                    Bucket: config.aws.s3.bucket,
-                    CopySource: `${config.aws.s3.bucket}/${record.s3.object.key}`,
-                    Key: record.s3.object.key.replace(config.aws.s3.uploadFolderName, config.aws.s3.parsedFolderName)
-                }).promise();
-
-                console.log(`Copied to ${config.aws.s3.bucket}/${config.aws.s3.parsedFolderName}`);
-
-                res();
-            });
-        });
+        /* Delete */
+        console.log(`Delete from ${config.aws.s3.bucket}/${record.s3.object.key}`);
+        await s3.deleteObject({
+            Bucket: config.aws.s3.bucket,
+            Key: initialPath
+        }).promise();
+        console.log(`Deleted from uploaded`);
     }
 };
